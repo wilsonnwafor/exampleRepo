875ff3de7bfc47b2cba18c0406fb06d488011d79
wilsonnwafor <nwaforobumneme12@gmail.com>
2024-10-29 22:31:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,171 @@
+// let btn = document.getElementById('button')
+// let imagebtn = document.getElementById('btn')
+// let imagebtn2 = document.getElementById('btn2')
+
+
+
+// imagebtn.addEventListener('click', function(){
+//   document.getElementById('img').src='download (6).jpeg'
+// })
+
+// imagebtn2.addEventListener('click', function(){
+//   document.getElementById('img').src='download (1).jpeg'
+// })
+
+
+
+// btn.addEventListener('click', function(){
+//   document.getElementById('header').style.fontSize='70px'
+//   console.log('Sucessfully Finished')
+  
+// })
+
+
+// const cars = ["BMW", 20, true];
+
+// console.log(cars);
+
+// console.log("Cars");
+
+// const obj = { carName: "Volvo", Quantity: 2, isGood: false }
+
+// console.log(obj)
+
+
+// const data = [10, ["red", "green", "blue"], "Mercedes"];
+
+// console.log(data);
+
+// const obj_data = {
+//   carName: "Mercedes",
+//   Quantity: 2,
+//   colors: ["red", "blue"]
+// }
+// console.log(obj_data);
+
+// const users = {
+//   userNames: ["ken_eddy", "bid_daddy_00", "sunny"],
+//   passwords: ["123456", "qwerty", "09876"],
+//   email: ["ken_eddy@gmail.com", "bid_daddy_00@gmail.com", "sunny@gmail.com"]
+// }
+
+// console.log(users);
+
+
+// let a, b;
+
+// a = 0.1;
+// b = 0.2;
+
+// console.log((a*10 + b*10) / 10);
+
+// let x = "Apple";
+// let y = 10;
+// console.log(x / y);
+
+// let r = NaN
+// console.log(typeof(r));
+
+
+// let myNumber = 50.600;
+
+// console.log(parseInt(myNumber))
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("10"));
+// console.log(Number("10.5"));
+// console.log(Number("10 5"));
+// console.log(Number("10,5"));
+// console.log(Number("John Doe"));
+// console.log(Number("10    "));
+// console.log(Number("  10    "));
+
+
+
+// const today = new Date().getDay()
+// // today = [0, 1, 2, 3, 4, 5, 6]
+
+
+
+// if(today == 0){
+//   console.log("Yay, it's Sunday!");
+// }else if(today == 1){
+//   console.log("Yay, It's Monday");
+// }else if(today == 2){
+//   console.log("It's Tuesday")
+// }else if(today == 3){
+//   console.log("it's Wednesday");
+// }else if(today == 4){
+//   console.log("it's Thursday")
+// }else if(today == 5){
+//   console.log("it's Friday");
+// }else if(today == 6){
+//   console.log("It's Saturday!")
+// }
+// else{
+//   console.log("Don't know what day it is!!!");
+  
+// };
+
+// let btn = document.getElementById("button");
+
+// let input = document.getElementById("inputBox");
+
+
+// function greeting(name) {
+//   console.log("Welcome", name);
+// };
+
+// btn.addEventListener("click", function () {
+//   let val = input.value;
+//   greeting(val);
+  
+// });
+
+
+
+// const cars = ['Benz', 'Volvo', 'Bmw', 'Nissan', 'Bentley'];
+// //                0       1       2       3
+// let text = "";
+
+// for(let i = 0; i < cars.length; i++){
+
+//   text += "<li>" + cars[i] + "</li>"
+  
+// }
+
+// document.getElementById('demo').innerHTML = text;
+
+
+let i = 0
+
+while(i < 5){
+
+  console.log(i);
+
+  i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
